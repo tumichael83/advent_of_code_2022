b5cd6222c3aca96cf7e459a4efeee1b6e696cdaf
michael tu <michael.tu@yale.edu>
2022-12-23 19:43:23 -0600

    finished 6.2

--- day6/day6.py
+++ day6/day6.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     with open(os.path.expanduser('~/Desktop/Advent of Code 2022/day6/input.txt'), 'r') as lines:
         input = lines.readline()
 
-        for i in range(len(input) - 4):
-            if len(set([c for c in input[i:i+4]])) == 4:
-                print(i+4)
+        for i in range(len(input) - 14):
+            if len(set([c for c in input[i:i+14]])) == 14:
+                print(i+14)
                 break
